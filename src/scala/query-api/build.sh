314767b79a491efed6bb4cc5e764c0fd3f4ffb14
ruchirj <ruchir.jha@gmail.com>
2025-07-25 17:07:22 -0700

    Docker fixes

--- src/scala/query-api/build.sh
+++ src/scala/query-api/build.sh
@@ -3,7 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 export GIT_SHA=$(git rev-parse --short HEAD)
 
