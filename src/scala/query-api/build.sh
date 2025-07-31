d1e988879ed5d04877ac92d32accd278f6a49b46
Michael Graff <mgraff@cardinalhq.io>
2025-07-30 23:28:17 -0500

    build and push

--- src/scala/query-api/build.sh
+++ src/scala/query-api/build.sh
@@ -9,8 +9,10 @@ export GIT_SHA=$(git rev-parse --short HEAD)
 ../gradlew clean build
 
 docker buildx build \
+  --pull \
+  --push \
   --platform linux/amd64,linux/arm64 \
   -f Dockerfile.local \
   -t public.ecr.aws/cardinalhq.io/lakerunner/query-api:latest .
 
-docker push public.ecr.aws/cardinalhq.io/lakerunner/query-api:latest
+#docker push public.ecr.aws/cardinalhq.io/lakerunner/query-api:latest
