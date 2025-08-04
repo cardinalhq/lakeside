1feb99368e0c4f7cf49d7559c091dc389bbce9a6
Michael Graff <mgraff@cardinalhq.io>
2025-08-03 21:01:37 -0500

    add target for latest-dev tag

---
+++ src/scala/query-api/build-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+export GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+export GIT_SHA=$(git rev-parse --short HEAD)
+
+../gradlew clean build
+
+docker buildx build \
+  --pull \
+  --push \
+  --platform linux/amd64,linux/arm64 \
+  -f Dockerfile.local \
+  -t public.ecr.aws/cardinalhq.io/lakerunner/query-api:latest-dev .
