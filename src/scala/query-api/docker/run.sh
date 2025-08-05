10d6181397ab297a2ec9c84d2a400b13a260fe65
Michael Graff <explorer@flame.org>
2025-08-04 21:14:38 -0500

    single docker, conditionally enable otlp export

---
+++ src/scala/query-api/docker/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+extrajars=""
+
+baseargs= \
+  -XX:+ExitOnOutOfMemoryError \
+  -XX:+UnlockDiagnosticVMOptions \
+  -XX:+DebugNonSafepoints \
+  -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -Dcom.sun.management.jmxremote.port=9875 \
+  -Dcom.sun.management.jmxremote.rmi.port=9875 \
+  -Djava.rmi.server.hostname=localhost \
+  org.springframework.boot.loader.JarLauncher
+
+
+if [ "$ENABLE_OTLP_TELEMETRY" = "true" ]; then
+  extrajars="${extrajars} -javaagent:/app/libs/otel-javaagent.jar"
+fi
+
+exec java ${baseargs} ${extrajars} "$@"
